5f620b005f56924f467c39cdb9a57aa1acdebd4f
Pushc <94675712+Pushc@users.noreply.github.com>
2025-05-27 12:55:46 +0530

    Update CrossConfig.java

--- payment/src/main/java/com/pushkar/PaymentGateway/Configuration/CrossConfig.java
+++ payment/src/main/java/com/pushkar/PaymentGateway/Configuration/CrossConfig.java
@@ -14,9 +14,9 @@ public class CrossConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**").allowedOrigins("http://127.0.0.1:5500","http://localhost:5500","https://pc-mart.netlify.app").allowedMethods("POST","GET","PUT","DELETE","PATCH","OPTIONS").allowedHeaders("*").allowCredentials(true);
+                registry.addMapping("/api/**").allowedOrigins("http://127.0.0.1:5500","http://localhost:5500","https://pc-mart.netlify.app","https://pcc-mart.netlify.app").allowedMethods("POST","GET","PUT","DELETE","PATCH","OPTIONS").allowedHeaders("*").allowCredentials(true);
 
-                registry.addMapping("/admin/**").allowedOrigins("http://127.0.0.1:5500","http://localhost:5500","https://pc-mart.netlify.app").allowedMethods("POST","GET","PUT","DELETE","PATCH","OPTIONS").allowedHeaders("*").allowCredentials(true);
+                registry.addMapping("/admin/**").allowedOrigins("http://127.0.0.1:5500","http://localhost:5500","https://pc-mart.netlify.app","https://pcc-mart.netlify.app").allowedMethods("POST","GET","PUT","DELETE","PATCH","OPTIONS").allowedHeaders("*").allowCredentials(true);
             }
         };
     }
