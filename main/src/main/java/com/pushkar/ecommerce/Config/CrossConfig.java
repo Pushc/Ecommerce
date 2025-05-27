19b0a10f9e051ee09d9dd3144153f739cf26a582
Pushc <94675712+Pushc@users.noreply.github.com>
2025-05-27 12:54:07 +0530

    added pcc-mart

--- main/src/main/java/com/pushkar/ecommerce/Config/CrossConfig.java
+++ main/src/main/java/com/pushkar/ecommerce/Config/CrossConfig.java
@@ -15,7 +15,7 @@ public class CrossConfig implements WebMvcConfigurer {
         System.out.println("Adding CORS Configuration");
 
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500","https://pc-mart.netlify.app") // Allowed frontend origins
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500","https://pc-mart.netlify.app""https://pcc-mart.netlify.app") // Allowed frontend origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow cookies and authorization headers
